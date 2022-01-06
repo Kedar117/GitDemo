4b1866ab8565af3acbef5e616872dde53317998f
kedar <kedarbojjawar@gmail.com>
2022-01-06 12:24:31 +0530

    changes made by US guy

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -10,6 +10,12 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("psotJira2");
 		System.out.println("psotJira3");
+		
+		System.out.println("psotJira4");
+		System.out.println("psotJira5");
+		System.out.println("psotJira6");
+		System.out.println("psotJira7");
+		
 	}
 	
 	@Test
